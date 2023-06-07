7a43d65c37196250f17ac5d0551f214519986f95
zula <hasanovazula519.com>
2023-06-07 17:30:56 +0500

    сумма от 1 до а. произведение от 1 до а.кол-во цифр в числе.массив из 8 элементов(внутри 0 и 1)

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*  Напишите программу,
+ которая выводит массив из 8 элеменотов, 
+ заполненный нулями и единицами в случайном порядке.  */
+
+ int[] array = new int[8];
+ void RandomArray(int[] array)
+ {
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(2);
+        Console.Write($"{array[i]} ");
+    }
+ }
+ RandomArray(array);
